44d0b0e19d3a5f848a33507032b115fc3ba9ddfe
arase <arase.taisuke@gmail.com>
2021-10-12 00:04:04 +0900

    [fix] 不要なテストを削除

--- tests/hello.test.ts
+++
@@ -1,9 +0,0 @@
-import { greeter } from '../src/main'
-
-describe(greeter.name, () => {
-  it('should return greeting', () => {
-    const greeting = greeter('John')
-
-    expect(greeting).toBe('Hello, John!')
-  })
-})
